9bf93a2c61e29056f5b08e3c6884be27a0f429d6
MinJeongH <gkaalswjd2@gmail.com>
2021-10-08 11:28:50 +0900

    workplace upload

---
+++ Hello/Hello.java
@@ -0,0 +1,22 @@
+/*
+Filepath : Workspace\Hello.java
+Filename : Hello.java
+--------------------------------
+Compile : javac Hello.java
+Run : java Hello
+--------------------------------
+class : 자바는 클래스 단위로 적용
+main : 진입점 (시작점)
+---------------------------------
+한글 사용 시 인코딩 -> 문자집합 -> 한국어 -> EUC-KR로 변경
+*/
+
+// REMARK : This code is sample.
+
+public class Hello {
+	public static void main (String[] args) {
+		System.out.println("Hello, java!");
+		System.out.println("Wellcome to java");
+		System.out.println("안녕 자바");
+	}
+}
